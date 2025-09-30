2afc32fa5674d5de5e7b7947fd7036dbd39d20da
James Wedgwood <jtbwedgwood@gmail.com>
2025-09-29 23:27:15 -0400

    Fix script

--- code/bash/DUAL-step1.sh
+++ code/bash/DUAL-step1.sh
@@ -1,11 +1,8 @@
-WORLD_SIZE=1 CUDA_VISIBLE_DEVICES=0,1,2,3 python step1/DUAL-step1.py   \
-    --model_path "Liuwenhao2022/RAHF-SFT" \
-    --data_path  "../data/ultrafeedback/rm" \
-    --output_dir "../model/DUAL/good" \
-    --preference_type "chosen"
+export WORLD_SIZE=1
+python step1/DUAL-step1.py --model_path "Liuwenhao2022/RAHF-SFT" \
+  --data_path "../data/ultrafeedback/rm" --output_dir "../model/DUAL/good" \
+  --preference_type "chosen"
 
-WORLD_SIZE=1 CUDA_VISIBLE_DEVICES=0,1,2,3 python step1/DUAL-step1.py   \
-    --model_path "Liuwenhao2022/RAHF-SFT" \
-    --data_path  "../data/ultrafeedback/rm" \
-    --output_dir "../model/DUAL/bad" \
-    --preference_type "rejected"
+python step1/DUAL-step1.py --model_path "Liuwenhao2022/RAHF-SFT" \
+  --data_path "../data/ultrafeedback/rm" --output_dir "../model/DUAL/bad" \
+  --preference_type "rejected"
