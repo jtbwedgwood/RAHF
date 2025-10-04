dcd541f86b3435e091b3b79933bba08406e32389
James Wedgwood <jtbwedgwood@gmail.com>
2025-10-04 16:17:47 +0000

    Versioning fixes

--- code/bash/SCIT-step1.sh
+++ code/bash/SCIT-step1.sh
@@ -2,15 +2,15 @@
 set -euo pipefail
 
 # Load environment variables (must be in same dir or specify full path)
-source "$(dirname "$0")/.env.sh"
+source "$(dirname "$0")/env.sh"
 
 echo "Using bucket: $S3_BUCKET_URI"
 echo "Sending alerts to: $ALERT_EMAIL_TO"
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python step1/SCIT-step1.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python /root/RAHF/code/step1/SCIT-step1.py \
     --base_model  "Liuwenhao2022/RAHF-SFT" \
     --seed 42 \
-    --data_path "../data/ultrafeedback/rm" \
+    --data_path "/root/RAHF/data/ultrafeedback/rm" \
     --batch_size 64 \
     --micro_batch_size 2 \
     --num_epochs 2 \
@@ -18,5 +18,5 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python step1/SCIT-step1.py \
     --max_length 768 \
     --warmup_ratio 0.1 \
     --save_steps 400 \
-    --output_dir '../model/SCIT/hir' 
+    --output_dir '/root/RAHF/model/SCIT/hir' 
 
